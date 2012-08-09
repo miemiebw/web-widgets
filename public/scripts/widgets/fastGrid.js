c1eb6219f35a494daf7e378038b1bfecf011f2ff
meimeibw <miemiebw@me.com>
2012-08-09 16:41:07 +0800

    add fastGrid files

--- public/scripts/widgets/fastGrid.js
+++ public/scripts/widgets/fastGrid.js
@@ -2,5 +2,36 @@
  * User: meimeibw
  * Date: 12-8-9
  * Time: 下午2:03
- * To change this template use File | Settings | File Templates.
  */
+
+!function(){
+    var FastGrid = function(element, options){
+
+    };
+
+    FastGrid.prototype = {
+        populate: function(){
+
+        }
+    };
+
+    $.fn.fastGrid = function(option){
+        return this.each(function(){
+            var $this = $(this)
+                , data = $this.data('fastGrid')
+                , options = $.extend({}, $.fn.fastGrid.defaults, typeof option == 'object' && option);
+            if (!data) $this.data('fastGrid', (data = new FastGrid(this, options)))
+        });
+    };
+
+    $.fn.fastGrid.defaults = {
+        width: 'auto',
+        height: 256,
+        url: false,
+        params: false, //可以是object也可以是function
+        colModule: [],
+        autoLoad: true
+    };
+
+    $.fn.fastGrid.Constructor = FastGrid;
+}(window.jQuery);

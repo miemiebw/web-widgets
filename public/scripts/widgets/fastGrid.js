6ac9157924f0a8f64dd2b67fb27d30b5f52ddda1
eric lee <tsheep.lee@gmail.com>
2012-08-10 00:06:11 +0800

    code fastGrid

--- public/scripts/widgets/fastGrid.js
+++ public/scripts/widgets/fastGrid.js
@@ -6,12 +6,96 @@
 
 !function(){
     var FastGrid = function(element, options){
+        var $el = $(element);
+        this.opts = options;
 
+        var $elParent = $el.parent();
+        $el.detach();
+        this.$fastGrid = $('<div class="fastGrid"></div>');
+        this.$headWrapper = $('<div class="headWrapper"></div>').appendTo(this.$fastGrid);
+        this.$thead = $('<thead></thead>').wrap('<table class="tableHead"></table>');
+        this.$thead.parent().appendTo(this.$headWrapper);
+        this.$bodyWrapper = $('<div class="bodyWrapper"></div>').appendTo(this.$fastGrid);
+        this.$tbody = $('<tbody></tbody>').appendTo($el);
+        $el.addClass('tableBody').appendTo(this.$bodyWrapper);
+        $elParent.append(this.$fastGrid);
+
+        this.init();
+        this.populate();
     };
 
     FastGrid.prototype = {
-        populate: function(){
 
+        init: function(){
+            var $thead = this.$thead;
+            var opts = this.opts;
+            //设置高宽
+            this.$fastGrid.width(opts.width).height(opts.height);
+
+            var $tr = $('<tr></tr>');
+            if(opts.cols.length != 0){
+                var contentWidth = 0;
+                $.each(opts.cols, function(index, col){
+                    var $th = $('<th></th>');
+                    if(index === 0){
+                        $th.addClass('first');
+                    }
+                    if(index === opts.cols.length-1){
+                        $th.addClass('last');
+                    }
+                    if(col.title){
+                        $th.html(col.title);
+                    }
+                    if(col.width){
+                        if(index === 0){
+                            contentWidth += col.width+2;
+                        }else{
+                            contentWidth += col.width+1;
+                        }
+
+                        $th.width(col.width);
+                    }
+                    $tr.append($th);
+                });
+                this.$headWrapper.width(contentWidth);
+            }
+            $thead.append($tr);
+        },
+
+        populate: function(){
+            var $tbody = this.$tbody;//这里最好是先detach,以提高性能
+            var opts = this.opts;
+
+            if(opts.items.length != 0){
+                $.each(opts.items, function(rowIndex, row){
+                    var $tr = $('<tr></tr>');
+                    $.each(opts.cols, function(colIndex, col){
+                        var $div = $('<div></div>')
+                            .html(row[col.name]);
+                        var $td = $('<td></td>');
+
+
+                        if(colIndex === 0){
+                            $td.addClass('first');
+                        }
+                        if(colIndex === opts.cols.length-1){
+                            $td.addClass('last');
+                        }
+                        if(rowIndex === 0){
+                            $td.addClass('topLine');
+                        }
+
+
+                        $td.width(col.width)
+                            .append($div);
+
+                        $tr.append($td);
+                    });
+                    $tbody.append($tr);
+                });
+
+            }
+            $tbody.parent().width(this.$headWrapper.width());
         }
     };
 
@@ -26,10 +110,11 @@
 
     $.fn.fastGrid.defaults = {
         width: 'auto',
-        height: 256,
+        height: '256px',
         url: false,
         params: false, //可以是object也可以是function
-        colModule: [],
+        items: [],
+        cols: [],
         autoLoad: true
     };
 
